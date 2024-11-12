c53cd11d3aeae792ef05893e8477867d3c36448e
waleed-awan <waleedcine007@gmail.com>
2024-11-12 17:13:20 +0500

    Some Changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -25,7 +25,7 @@ export function Footer() {
               </div>
             </div>
             <span className="text-sm text-zinc-300">
-              © Developed by João Knisspell
+              © Developed by Waleed Awan
             </span>
           </div>
           <div className="flex flex-col gap-4">
